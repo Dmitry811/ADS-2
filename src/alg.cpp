f7c81c6e7ad69ab350e1076ea0867da45e21969c
Dmitry811 <digonin288@gmail.com>
2024-02-28 20:25:55 +0300

    ADS-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,49 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double iCount = 1;
+    for (uint16_t i = 1; i <= n ; i++){
+        iCount *= value;
+    }
+    return iCount;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint16_t iCount1 = 1;
+    for (uint16_t i = 1; i <= n; i++){
+        iCount1 *= i;
+    }
+    return iCount1;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+    double iCount2 = 1;
+    for (uint16_t i = 1; i <= count; i++){
+        iCount2 += calcItem(x,i);
+    }
+    return iCount2;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double iCount3 = 0;
+    int chet = 1;
+    for (uint16_t i = 1; i <= count; i++){
+        iCount3 += (pown(-1,i-1)) * (pown(x,chet) / fact(chet));
+        chet += 2;
+    }
+    return iCount3;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double iCount4 = 1;
+    int chet1 = 2;
+    for (uint16_t i = 1; i <= count; i++){
+        iCount4 += (pown(-1,i)) * (pown(x,chet1) / fact(chet1));
+        chet1 += 2;
+    }
+    return iCount4;
 }
