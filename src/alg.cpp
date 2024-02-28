98afc3460bef30bf6b13c1b62e4bf070aaf870a8
Dmitry811 <digonin288@gmail.com>
2024-02-28 21:16:50 +0300

    ADS-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,15 +5,15 @@
 
 double pown(double value, uint16_t n) {
     double iCount = 1;
-    for (uint16_t i = 1; i <= n ; i++){
+    for (uint16_t i = 1; i <= n ; i++) {
         iCount *= value;
     }
     return iCount;
 }
 
 uint64_t fact(uint16_t n) {
-    uint16_t iCount1 = 1;
-    for (uint16_t i = 1; i <= n; i++){
+    uint64_t iCount1 = 1;
+    for (uint16_t i = 1; i <= n; i++) {
         iCount1 *= i;
     }
     return iCount1;
@@ -24,18 +24,18 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double iCount2 = 1;
-    for (uint16_t i = 1; i <= count; i++){
-        iCount2 += calcItem(x,i);
+    double ex = 1;
+    for (uint16_t i = 1; i <= count; ++i) {
+        ex += calcItem(x, i);
     }
-    return iCount2;
+    return ex;
 }
 
 double sinn(double x, uint16_t count) {
     double iCount3 = 0;
     int chet = 1;
-    for (uint16_t i = 1; i <= count; i++){
-        iCount3 += (pown(-1,i-1)) * (pown(x,chet) / fact(chet));
+    for (uint16_t i = 1; i <= count; i++) {
+        iCount3 += (i % 2 == 0 ? -1 : 1) * (pown(x, chet) / fact(chet));
         chet += 2;
     }
     return iCount3;
@@ -44,8 +44,8 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
     double iCount4 = 1;
     int chet1 = 2;
-    for (uint16_t i = 1; i <= count; i++){
-        iCount4 += (pown(-1,i)) * (pown(x,chet1) / fact(chet1));
+    for (uint16_t i = 1; i <= count; i++) {
+        iCount4 += (i % 2 == 0 ? 1 : -1) * (pown(x, chet1) / fact(chet1));
         chet1 += 2;
     }
     return iCount4;
